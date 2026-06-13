15404bd8f0c133774bb173aa34aa753a8af8f4d6
Shahid chopda <118037269+Shahidchopda@users.noreply.github.com>
2026-06-13 20:48:46 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,17 @@
-const CACHE = 'runtrack-v1';
-const ASSETS = ['./index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+const CACHE='runtrack-v3';
+const ASSETS=['./index.html','./manifest.json'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
-  );
+self.addEventListener('fetch',e=>{
+  if(e.request.url.includes('tile.openstreetmap.org')||e.request.url.includes('leaflet')){
+    e.respondWith(caches.open(CACHE).then(c=>c.match(e.request).then(r=>r||fetch(e.request).then(res=>{c.put(e.request,res.clone());return res;}))));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>r)));
 });
